c51635058c8b45f3d3dbf258ba7eb2f237deada2
shiyoulin <yulishi@beanfun.com>
2022-03-24 11:01:12 +0800

    取消compose依賴

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id ("kotlin-android")
 
     kotlin("android")
 }
@@ -10,11 +9,6 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.compose.material:material:1.1.1")
-    implementation("androidx.compose.animation:animation:1.1.1")
-    implementation("androidx.compose.ui:ui-tooling:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
     implementation(Develop.Ktor.androidCore)
     implementation(Develop.Coroutines.common)
     implementation(Develop.Coroutines.android)
@@ -25,11 +19,11 @@ dependencies {
 }
 
 android {
-    compileSdkVersion(31)
+    compileSdkVersion(30)
     defaultConfig {
         applicationId = "com.officeyuli.kmmexampleforitfinal.android"
         minSdkVersion(23)
-        targetSdkVersion(31)
+        targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
     }
@@ -38,17 +32,5 @@ android {
             isMinifyEnabled = false
         }
     }
-    buildFeatures{
-        compose =  true
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility =  JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.1"
-    }
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("com.android.tools.build:gradle:7.0.1")
         classpath(Classpath.kotlin_serialization)
 
