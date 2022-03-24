097eb917257283b2934b07f686eb191c7047298b
shiyoulin <yulishi@beanfun.com>
2022-03-24 10:48:10 +0800

    加入compose 依賴

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+    id ("kotlin-android")
+
     kotlin("android")
 }
 
@@ -8,6 +10,11 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.compose.material:material:1.1.1")
+    implementation("androidx.compose.animation:animation:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
     implementation(Develop.Ktor.androidCore)
     implementation(Develop.Coroutines.common)
     implementation(Develop.Coroutines.android)
@@ -18,11 +25,11 @@ dependencies {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdkVersion(31)
     defaultConfig {
         applicationId = "com.officeyuli.kmmexampleforitfinal.android"
         minSdkVersion(23)
-        targetSdkVersion(30)
+        targetSdkVersion(31)
         versionCode = 1
         versionName = "1.0"
     }
@@ -30,5 +37,18 @@ android {
         getByName("release") {
             isMinifyEnabled = false
         }
+    }
+    buildFeatures{
+        compose =  true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility =  JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
     }
 }
